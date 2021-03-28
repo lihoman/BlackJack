61403229dc13d5a3e1e3082557a7ee02c51e431e
lihoman <mikita.likhamanau@gmail.com>
2021-03-29 00:51:15 +0300

    starting work with server, but currently not working code (problem with displaying (decoding)  cards in client console)

--- client.py
+++ client.py
@@ -1,9 +1,32 @@
 import socket
+import json
+from Cards import *
 
-sock_client = socket.socket()
+def info_from_json(json_info):
+    python_format = json.loads(json_info.decode('utf-8'))
+    return python_format
+
+
+def info_in_json(*info):
+    info = json.dumps(info)
+    return info.encode('utf-8')
+
+
+sock_client = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM,
+)
 sock_client.connect(('localhost', 10000))
 
 while True:
-    break
+    player_name = input('Welcome in BlackJack. What is your name?: ')  # give info about client
+    sock_client.send(info_in_json(player_name))  # sending client name to server to create a player
+    player_count = info_from_json(sock_client.recv(1024))  # receive info about client count in game
+    print(player_count)
+    player_rate = input("Please, make your bet: ")  # client rate in game
+    sock_client.send(info_in_json(player_rate))  # sending info about client rate to server
+    player_cards = info_from_json(sock_client.recv(1024))  # receive info what cards has client
+    player_result = info_from_json(sock_client.recv(1024))
+    print(player_cards, f'{player_name} result: {player_result}', sep='\n')
 
-sock_client.close()
+# sock_client.close()
--- server.py
+++ server.py
@@ -1,9 +1,21 @@
 import socket
+import json
+from GameRound import *
 
 
 def write_in_log(info):
     with open('server_info.log', 'a') as f:
         f.write(info + '\n')
+
+
+def info_in_json(*info):
+    info = json.dumps(info)
+    return info.encode('utf-8')
+
+
+def info_from_json(info):
+    info = json.loads(info.decode('utf-8'))
+    return info
 
 
 server = socket.socket(
@@ -17,7 +29,15 @@ client_sock, client_addr = server.accept()
 
 write_in_log(f'Client {client_addr[0]} connected')
 
-while True:
-    break
 
-client_sock.close()
+while True:
+    player_name = info_from_json(client_sock.recv(1024))  # receive info about client name
+    game = GameRound(player_name)  # create player and dealer
+    client_sock.send(info_in_json(game.player.count))  # send info about how much money client has
+    player_rate = info_from_json(client_sock.recv(1024))  # receive info about client's rate
+    game.player.my_cards = game.player.generate_start_cards()  # start cards for client
+    game.player_result = game.player.finish_result()  # view in number what sum of cards client has
+    client_sock.send(info_in_json(game.player.my_cards))  # send this info
+    client_sock.send(info_in_json(game.player_result))
+
+# client_sock.close()
